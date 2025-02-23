ceea5a69833df080cd07b36d2843caae715fb972
Gabriel Fontes <bielfontes6782@gmail.com>
2025-02-23 20:08:57 -0300

    Feat: botoes menu

--- aplicacao/src/app/pages/home-pages/HomePages/home-menu/home-menu.component.ts
+++ aplicacao/src/app/pages/home-pages/HomePages/home-menu/home-menu.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';  // Importando CommonModule
 
 @Component({
   selector: 'app-home-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],  // Adicionando CommonModule aqui
   templateUrl: './home-menu.component.html',
-  styleUrl: './home-menu.component.scss'
+  styleUrls: ['./home-menu.component.scss']  // Corrigido para styleUrls
 })
 export class HomeMenuComponent {
+  activeButton: string = 'Home';  // Armazena o botão ativo
 
+  // Método para definir o botão ativo
+  setActiveButton(button: string) {
+    this.activeButton = button;
+  }
+
+  // Verifica se o botão está ativo
+  isActive(button: string): boolean {
+    return this.activeButton === button;
+  }
 }
